5246a996152339808fb7eb4f12c031e53c88b920
Tsvetomir Valchev <92424452+TsvetomirValchev@users.noreply.github.com>
2023-04-05 10:26:05 +0300

    Update LineSwapper.java

--- src/main/java/FileManagers/LineSwapper.java
+++ src/main/java/FileManagers/LineSwapper.java
@@ -3,7 +3,7 @@ package FileManagers;
 import java.util.List;
 
 public class LineSwapper {
-    //This method swaps two lines in a List<List<String>> text from their indexes(they start from 0)
+    //This method swaps two lines in a List<List<String>> text by their indexes(they start from 0)
     public static void swapLines(List<List<String>> text,  int firstLine, int secondLine) {
         List<String> temp = text.get(firstLine);
         text.set(firstLine, text.get(secondLine));
